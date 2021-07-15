200dc1527490585dc261f5813e23cba64a74d29d
musung <musungkim1352@gmail.com>
2021-07-15 10:40:55 +0900

    add new channel

--- lib/controllers/audioChatController.dart
+++ lib/controllers/audioChatController.dart
@@ -10,33 +10,45 @@ const APP_ID = 'c1886a27a9af440cbca306eda4b3047e';
 class AudioController extends GetxController{
   static AudioController instance = AudioController();
   TextEditingController channelName = TextEditingController();
-  RxList<String> channelNames = <String>[].obs; // 모든 채널 이름 저장
-  RxList<RtcChannel> channels = <RtcChannel>[].obs; // 자신이 만든 채널 저장
+  RxList<RoomModel> channels = <RoomModel>[].obs; // 모든 채널 이름 저장
   late RtcEngine engine;
   @override
   void onReady() {
     initPlatformState();
-    //channelNames.bindStream(getChannel());
+    channels.bindStream(getChannel());
     super.onReady();
   }
-  Future<void>initPlatformState()async{
+  Future<void>initPlatformState()async{ // 엔진 초기화
     await [Permission.microphone].request();
     RtcEngineConfig config = RtcEngineConfig(APP_ID);
     engine = await RtcEngine.createWithConfig(config);
-    await engine.setClientRole(ClientRole.Audience);
   }
-  Future<void>makeChannel()async{
-    channels.add(await RtcChannel.create(channelName.text));
+  Future<void>makeChannel()async{ // 채널 생성
+    await engine.setClientRole(ClientRole.Broadcaster);
+    await engine.joinChannel(null, channelName.text,null,0);
+
   }
-  addChannelNameToFirebase(){
-    firebaseFirestore.collection("channelName").doc(channelName.text).set({
+  addChannelToFirebase()async{ // 채널 이름 저장
+    await firebaseFirestore.collection("channelName").doc(channelName.text).set({
       'channel' : channelName.text,
     });
-  // }
-  // Stream <String> getChannel() =>
-  //     firebaseFirestore.collection("channelName").snapshots().map((value) =>
-  //         value.docs.map((item)=>
-  //             RoomModel.fromMap(item.data())
-  //         ).toList()
-  //     );
+  }
+  joinChannel(String channelName)async{  // 채널 입장
+    await engine.setClientRole(ClientRole.Audience);
+    await engine.joinChannel(null,channelName,null,0);
+  }
+  void dispose()async{ //
+    await engine.leaveChannel();
+    //_client.logout();
+    //_client.destroy();
+    //_seniorMember.clear();
+    //_channelList.clear();
+    super.dispose();
+  }
+  Stream <List<RoomModel>> getChannel()=>  // roomModel 클라우드에서 받아오기
+      firebaseFirestore.collection("channelName").snapshots().map((value) =>
+          value.docs.map((item)=>
+              RoomModel.fromMap(item.data())
+          ).toList()
+      );
 }
